c861bb752c239b40874332a598da444d67b4a7ba
Luigi <66072698+lui-dias@users.noreply.github.com>
2023-09-30 08:19:14 -0300

    modified tailwind

---
+++ src/minify-stuff.js
@@ -0,0 +1,27 @@
+const letters = 'abcdefghijklmnopqrstuvwxyz'
+
+function mini() {
+	const n = [0]
+
+	return () => {
+		let s = ''
+
+		for (const i of n) {
+			s += letters[i]
+		}
+
+		const last = n[n.length - 1]
+
+		if (last < letters.length - 1) {
+			n[n.length - 1] = last + 1
+		} else {
+			n.push(0)
+		}
+
+		return s
+	}
+}
+
+module.exports = {
+    mini
+}
